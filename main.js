74e2dbbaad7db2cbfe0e1694928f7c5e3b04a3eb
Joe Antongiovanni <jantongiovanni@csumb.edu>
2018-02-21 02:47:43 +0000

    inclass code

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var OAuth = require('oauth');
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -27,6 +27,39 @@ io.on('connection', function(socket) {
   io.emit('chat message', "Please Enter a Keyword to See Twitter Posts");
   console.log('a user connected');
   socket.on('chat message', function(msg) {
+    
+        var oauth = new OAuth.OAuth(
+      'https://api.twitter.com/oauth/request_token',
+      'https://api.twitter.com/oauth/access_token',
+      process.env.oauth1,
+      process.env.oauth2,
+      '1.0A',
+      null,
+      'HMAC-SHA1'
+    );    var oauth = new OAuth.OAuth(
+      'https://api.twitter.com/oauth/request_token',
+      'https://api.twitter.com/oauth/access_token',
+      process.env.oauth1,
+      process.env.oauth2,
+      '1.0A',
+      null,
+      'HMAC-SHA1'
+    );
+    
+        oauth.get(
+      'https://api.twitter.com/1.1/search/tweets.json?q=' + msg + "&count=10",
+      process.env.oauth3,
+      process.env.oauth4,
+      function(e, data, res) {
+        console.log("twitter api call complete")
+        if (e) console.error(e);
+        data = JSON.parse(data)
+        console.log(data)
+        for (var i in data["statuses"]) {
+          io.emit('chat message', data["statuses"][i]["text"]);
+        }
+      });
+    
     io.emit('chat message', msg);
 
   });
